5bef19cac4d17c561d062aba6f8b228d034d6f7e
DavidBeckschulte <david_beckschulte@gmx.de>
2026-05-29 14:40:05 +0200

    added more stuff into perception.

--- ws/src/my_robot_perception/setup.py
+++ ws/src/my_robot_perception/setup.py
@@ -4,26 +4,18 @@ package_name = 'my_robot_perception'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='dbecksch',
-    maintainer_email='david.beckschulte@uni-muenster.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
     entry_points={
         'console_scripts': [
+            'goal_checker_node = my_robot_perception.goal_checker_node:main',
+            'scan_to_pointcloud_node = my_robot_perception.scan_to_pointcloud_node:main',
         ],
     },
 )
