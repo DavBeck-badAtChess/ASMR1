418cc0c00852a1dd6ee918e6d5f068c68e516a30
DavidBeckschulte <david_beckschulte@gmx.de>
2026-06-06 19:22:26 +0200

    stupefied the system a bit. it is now ownde by the meta controller, and only ever called from there

--- ws/src/my_robot_nav/my_robot_nav/map_test.py
+++ ws/src/my_robot_nav/my_robot_nav/map_test.py
@@ -5,31 +5,42 @@ import numpy as np
 
 
 class OccGrid(Node):
-    def __init__(self):
+    def __init__(self, map_dims_in_meter : tuple[float, float]):
+        '''
+        this class will take a map, and display it.
+        this is mainly for debug purpuses, but also because it is kinda cool
+        '''
+
+        self._map_dims_in_meter: tuple[float, float] = map_dims_in_meter
         super().__init__('map_test')
         self.pub = self.create_publisher(OccupancyGrid, 'map', 10)
-        self.timer = self.create_timer(1.0, self.publish)
 
-    def publish(self):
+
+
+    def display(self, tile_grid: np.ndarray):
+        '''
+        takes a map of the world, and displays it. thats it.
+        '''
         msg = OccupancyGrid()
 
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "map"
         
         msg.info.resolution = 1.0
-        msg.info.width = 5
-        msg.info.height = 5
+        msg.info.width = self._map_dims_in_meter[0]
+        msg.info.height = self._map_dims_in_meter[1]
         
-        msg.info.origin.position.x = 0.0
-        msg.info.origin.position.y = 0.0
+        # i want it to be at the center
+        msg.info.origin.position.x = self._map_dims_in_meter[0]/2
+        msg.info.origin.position.y = self._map_dims_in_meter[1]/2
         msg.info.origin.position.z = 0.0
         msg.info.origin.orientation.w = 1.0
         
-        grid = np.zeros((5, 5), dtype=np.int8)
-        grid[2, 2] = 100
-        grid[1, 3] = 50
+        # grid = np.zeros((5, 5), dtype=np.int8)
+        # grid[2, 2] = 100
+        # grid[1, 3] = 50
         
-        msg.data = grid.flatten().tolist()
+        msg.data = tile_grid.flatten().tolist()
         
         self.pub.publish(msg)
 
