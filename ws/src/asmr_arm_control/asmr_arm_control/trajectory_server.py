56bb3c97c58d9654a241c0eb55dd05c69cc3b4a1
DavidBeckschulte <david_beckschulte@gmx.de>
2026-06-28 11:30:55 +0200

    very naive navigation testing

--- ws/src/asmr_arm_control/asmr_arm_control/trajectory_server.py
+++ ws/src/asmr_arm_control/asmr_arm_control/trajectory_server.py
@@ -13,10 +13,12 @@ from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 
 from asmr_arm_interfaces.action import ExecuteTrajectory
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import JointState, MultiDOFCommand
 from asmr_arm_interfaces.srv import ComputeFK
 from asmr_arm_interfaces.srv import ComputeIK
 from asmr_arm_interfaces.srv import BSService
+
+
 
 import os
 from ament_index_python.packages import get_package_share_directory
@@ -33,6 +35,20 @@ float64 theta2   # elbow joint angle [rad]
 float64 x        # reach from the shoulder [m]
 float64 y        # height relative to the shoulder [m]
 bool success     # true if the request was valid
+'''
+
+'''
+ros2 topic pub /arm_pid_controller/reference control_msgs/msg/MultiDOFCommand "
+dof_names:
+  - elbow
+  - shoulder
+values:
+  - 2.0
+  - 2.0
+values_dot:
+  - 0.0
+  - 0.0
+"
 '''
 
 
@@ -54,8 +70,10 @@ class TrajectoryServer(Node):
         self.ik_req = ComputeIK.Request()
         self.current_theta1 = 0.0
         self.current_theta2 = 0.0
+        self._current_x = 0
+        self._current_y = 0
 
-        # Create service clients
+        # Create service clients=========================================================================
         self._fk_client = self.create_client(
             ComputeFK,
             "forward_kinematics",
@@ -64,6 +82,9 @@ class TrajectoryServer(Node):
             ComputeIK,
             "inverse_kinematics",
         )
+
+        # Create publisher ===============================================================================
+        self._cmd_pub = self.create_publisher(MultiDOFCommand, '/arm_pid_controller/reference', 10)
 
         while not self._fk_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("FK server not available, trying again")
@@ -109,18 +130,24 @@ class TrajectoryServer(Node):
         if self.__class__.DEBUG:self.get_logger().info(f"{self.__class__}::sending ik request x: {self.ik_req.x}, y: {self.ik_req.y} "+30*"-")
         return self._ik_client.call_async(self.ik_req)
 
-    # def _callback(self, request, response):
-    #     pass
 
     def joint_callback(self, msg):
         self.current_theta1 = msg.position[0]
         self.current_theta2 = msg.position[1]
-        
+
+
     def bs_callback(self, request, response):
         x_interpolation, y_interpolation = zip(*(self._plan_trajectory((request.x, request.y))))
         response.x_coords = x_interpolation
         response.y_coords = y_interpolation
         if self.__class__.DEBUG:self.get_logger().info(f"{self.__class__}::exiting bs_callback "+30*"-")
+
+    @staticmethod
+    def _get_multi_dof_cmd(theta1:float, theta2:float)->MultiDOFCommand:
+        ret_mdc = MultiDOFCommand()
+        ret_mdc.dof_names = ["elbow", "shoulder"]
+        ret_mdc.values = [theta1, theta2]
+        return ret_mdc
 
 
     def _plan_trajectory(self, end_pos: tuple[float, float]) -> list[tuple[float, float]]:
@@ -131,10 +158,7 @@ class TrajectoryServer(Node):
         
         if self.__class__.DEBUG:self.get_logger().info(f"{self.__class__}::Entered _plan_trajectory "+30*"-")
         future = self.send_fk_request(self.current_theta1, self.current_theta2)
-        #if self.__class__.DEBUG:self.get_logger().info(f"{self.__class__}::Entered _plan_trajectory "+30*"-")
-        #self.get_logger().info("request sent")
         rclpy.spin_until_future_complete(self, future)
-        #self.get_logger().info("finished spinning")
         response = future.result()
         current_pos = (response.x, response.y)
         x_interpolations = np.linspace(current_pos[0], end_pos[0], NO_INTERPOLATIONS)
@@ -170,8 +194,33 @@ class TrajectoryServer(Node):
                 return result
             
             # --- TODO ---
-            # command arm to waypoint and wait for it to arrive
-            
+            # command arm to waypoint and wait for it to arrive=======================================
+            point_reached = False
+            while not point_reached:
+                
+                # get ik target angles and send them
+                future = self.send_ik_request(self._current_x,self._current_y)
+                rclpy.spin_until_future_complete(self, future)
+                response = future.result()
+                target_theta_1 = response.theta1
+                target_theta_2 = response.theta2
+
+                # create teh request
+                cmd = TrajectoryServer._get_multi_dof_cmd(target_theta_1, target_theta_2)
+                self._cmd_pub.publish(cmd)
+                rclpy.spin_once(self, timeout_sec=0.1)
+                
+                
+                # determain current real position
+                future = self.send_fk_request(self.current_theta1, self.current_theta2)
+                rclpy.spin_until_future_complete(self, future)
+                response = future.result()
+                self._current_x = response.x
+                self._current_y = response.y
+                if np.linalg.norm(np.array([self._current_x,self._current_y])- np.array[waypoint[0],waypoint[1]]) < 0.01:point_reached = True
+
+            # /command arm to waypoint and wait for it to arrive=======================================
+
             # sending feedback
             feedback_msg.waypoint_index = i
             feedback_msg.ee_x = waypoint[0]
